53457672da830f68b720e061c0d2a1a07200882d
snovaisg <snovaisg.97@gmail.com>
2018-02-02 05:19:17 +0000

    Update top_kages.py
    stoopid 7th

--- UN_BOT/package/Top_Kages/top_kages.py
+++ UN_BOT/package/Top_Kages/top_kages.py
@@ -15,13 +15,13 @@ class top_kages:
 		sleep(2)
 
 	def practice(self, practice, ri):
-		kages = ['kazekage', 'mizukage', 'tsuchikage', 'raikage', 'hokage', '4th_hokage', '1st_hokage', 'mystical_shadow', '2nd_tsuchikage','6th_hokage']
+		kages = ['kazekage', 'mizukage', 'tsuchikage', 'raikage', 'hokage', '4th_hokage', '1st_hokage', 'mystical_shadow', '2nd_tsuchikage', '6th_hokage', '7th_hokage']
 		stop = len(kages) if practice == 'all' else kages.index(practice)+1
 
 		for e in range(0,stop):
 			self.mouse.move(self.positions.parse(_POSITIONS, 'top_kages', kages[e]))
 			sleep(1)
-			if (e >6 and not ri):
+			if (e >6 and !ri):
 				break
 		self.mouse.move(self.positions.parse(_POSITIONS, 'top_kages', 'close'))
 		sleep(1)
